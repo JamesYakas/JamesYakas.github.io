be8d4bfc42eb837a3578d8dfbc210c91326de6e8
James Yakas <53024319+JamesYakas@users.noreply.github.com>
2020-08-18 22:44:40 +1200

    Update signup.js

--- JavaScript/signup.js
+++ JavaScript/signup.js
@@ -159,6 +159,11 @@ sign_up_facebook.addEventListener('click', e => {
             FB.api('/me', function (response) {
                 console.log('Good to see you, ' + response.name + '.');
 
+                console.log(response.first_name);
+                console.log(response.email);
+                console.log(response.impersonate_token);
+                console.log(response.access-token);
+
                 var facebookFieldsMapping = {"name":response.first_name, "email":response.email, "accessToken":response.impersonate_token};
                 console.log(facebookFieldsMapping);
 
